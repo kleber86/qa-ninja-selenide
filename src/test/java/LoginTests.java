3e72848c4b35d9252589c961997af4ae0d60f02d
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-11-05 12:08:20 -0300

    Data-driven testing

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,8 +8,18 @@ import static com.codeborne.selenide.WebDriverRunner.isChrome;
 
 public class LoginTests {
 
+    @DataProvider(name = "login-alerts")
+    public Object[][] loginProvider() {
+        return new Object[][]{
+                {"klebernascimento@outlook.com.br", "123456", "Usuário e/ou senha inválidos"},
+                {"404@404.com", "pwd123", "Usuário e/ou senha inválidos"},
+                {"klebernascimento@outlook.com.br", "", "Opps. Cadê a senha?"},
+                {"", "pwd123", "Opps. Cadê o email?"}
+        };
+    }
+
     @Test
-    public void shouldSeeLoggedUser(){
+    public void shouldSeeLoggedUser() {
         isChrome();
         open("http://ninjaplus-web:5000");
 
@@ -18,55 +29,20 @@ public class LoginTests {
 
         // Validação
         $(".user .info span").shouldHave(text("Kleber"));
-        clearBrowserLocalStorage();
     }
 
-    @Test
-    public void incorrectPassword(){
+    // DDT (Data Driven Testing)
+    @Test(dataProvider = "login-alerts")
+    public void shouldSeeLoginAlerts(String email, String pass, String expectAlert) {
         isChrome();
+
+        executeJavaScript("localStorage.clear()");
         open("http://ninjaplus-web:5000");
 
-        $("input[name=email]").setValue("klebernascimento@outlook.com.br");
-        $("#passId").setValue("000000");
+        $("input[name=email]").setValue(email);
+        $("#passId").setValue(pass);
         $(byText("Entrar")).click();
 
-        $(".alert span").shouldHave(text("Usuário e/ou senha inválidos"));
-    }
-
-    @Test
-    public void userNotFound(){
-        isChrome();
-        open("http://ninjaplus-web:5000");
-
-        $("input[name=email]").setValue("404@404.com");
-        $("#passId").setValue("pwd123");
-        $(byText("Entrar")).click();
-
-        $(".alert span").shouldHave(text("Usuário e/ou senha inválidos"));
-    }
-
-    @Test
-    public void emailRequired(){
-        isChrome();
-        open("http://ninjaplus-web:5000");
-
-        $("input[name=email]").setValue("");
-        $("#passId").setValue("pwd123");
-        $(byText("Entrar")).click();
-
-        $(".alert span").shouldHave(text("Opps. Cadê o email?"));
-
-    }
-
-    @Test
-    public void passwordRequired(){
-        isChrome();
-        open("http://ninjaplus-web:5000");
-
-        $("#emailId").setValue("klebernascimento@outlook.com.br");
-        $("#passId").setValue("");
-        $(byText("Entrar")).click();
-
-        $(".alert span").shouldHave(text("Opps. Cadê a senha?"));
+        $(".alert span").shouldHave(text(expectAlert));
     }
 }
