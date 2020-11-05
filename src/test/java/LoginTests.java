3a192e77324ff4a466c2ef60a413d6373b4c4917
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-11-05 10:56:17 -0300

    Automatizando fluxos alternativos

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,8 +2,7 @@ import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.isChrome;
 
 public class LoginTests {
@@ -19,5 +18,55 @@ public class LoginTests {
 
         // Validação
         $(".user .info span").shouldHave(text("Kleber"));
+        clearBrowserLocalStorage();
+    }
+
+    @Test
+    public void incorrectPassword(){
+        isChrome();
+        open("http://ninjaplus-web:5000");
+
+        $("input[name=email]").setValue("klebernascimento@outlook.com.br");
+        $("#passId").setValue("000000");
+        $(byText("Entrar")).click();
+
+        $(".alert span").shouldHave(text("Usuário e/ou senha inválidos"));
+    }
+
+    @Test
+    public void userNotFound(){
+        isChrome();
+        open("http://ninjaplus-web:5000");
+
+        $("input[name=email]").setValue("404@404.com");
+        $("#passId").setValue("pwd123");
+        $(byText("Entrar")).click();
+
+        $(".alert span").shouldHave(text("Usuário e/ou senha inválidos"));
+    }
+
+    @Test
+    public void emailRequired(){
+        isChrome();
+        open("http://ninjaplus-web:5000");
+
+        $("input[name=email]").setValue("");
+        $("#passId").setValue("pwd123");
+        $(byText("Entrar")).click();
+
+        $(".alert span").shouldHave(text("Opps. Cadê o email?"));
+
+    }
+
+    @Test
+    public void passwordRequired(){
+        isChrome();
+        open("http://ninjaplus-web:5000");
+
+        $("#emailId").setValue("klebernascimento@outlook.com.br");
+        $("#passId").setValue("");
+        $(byText("Entrar")).click();
+
+        $(".alert span").shouldHave(text("Opps. Cadê a senha?"));
     }
 }
