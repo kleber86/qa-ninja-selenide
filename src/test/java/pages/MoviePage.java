292868902a41116de7260684f2d3b47c53241287
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-11-07 21:11:25 -0300

    Array de tags

--- src/test/java/pages/MoviePage.java
+++ src/test/java/pages/MoviePage.java
@@ -1,5 +1,10 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
+import java.util.List;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -11,10 +16,22 @@ public class MoviePage {
         return this;
     }
 
-    public MoviePage create(String title, String status){
+    public MoviePage create(String title, String status, String year, String releaseDate, List<String> cast, String plot){
         $("input[name=title]").setValue(title);
         this.selectStatus(status);
+        $("input[name=year]").setValue(year);
+        $("input[name=release_date]").setValue(releaseDate);
+        this.input(cast);
+        $("textarea[name=overview]").setValue(plot);
+
         return this;
+    }
+
+    private void input(List<String> cast){
+        SelenideElement element =  $(".cast");
+        for(String actor : cast){
+            element.setValue(actor).sendKeys(Keys.TAB);
+        }
     }
 
     private void selectStatus(String status){
--- src/test/java/tests/MovieTests.java
+++ src/test/java/tests/MovieTests.java
@@ -4,6 +4,9 @@ import common.BaseTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static com.codeborne.selenide.Condition.text;
 
 public class MovieTests extends BaseTest {
@@ -22,7 +25,13 @@ public class MovieTests extends BaseTest {
     public void shouldRegisterANewMovie() {
         String title = "Jumanji - Proxima Fase";
         String status = "Pré-venda";
+        String year = "2020";
+        String releasesDate = "16/01/2020";
+        List<String> cast = Arrays.asList("The Rock", "Jack Black", "Kevin Hart", "Karen Gillian", "Danny DeVito");
+        String plot = "Lorem Ipsum is simply dummy text of the printing and typesetting " +
+                "industry. Lorem Ipsum has been the industry's standard dummy text ever " +
+                "since the 1500s.";
 
-        movie.add().create(title, status);
+        movie.add().create(title, status, year, releasesDate, cast, plot);
     }
 }
